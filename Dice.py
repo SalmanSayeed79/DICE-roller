b1bb4f5bea45956835f741b1f7fc24fe0d319dd5
SalmanSayeed79 <66307068+SalmanSayeed79@users.noreply.github.com>
2020-06-14 23:08:31 +0600

    Added all the necessary files
    Didn't do anything else, just uploaded all the necessary files.

---
+++ Dice.py
@@ -0,0 +1,41 @@
+import random
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+def number():
+    a=random.randint(1,6)
+    print(a)
+    #name='{}.ico'.format(a)
+#file=Image.open(name)
+    #file=file.resize((60,60))
+    #files=ImageTk.PhotoImage(file)
+
+    #print(files)
+    label2=Label(frame1,text=a,bg='#C2F77B',font='Cambria')
+
+    label2.pack()
+root=Tk()
+root.geometry('500x500')
+root.configure(background="#F9E79F")
+root.title("Dice Roller")
+root.iconbitmap('main.ico')
+#a=Image.open('10.png')
+
+#image1=ImageTk.PhotoImage(Image.open('1.ico'))
+#sign=Label(root,image=image1)
+#sign.pack()
+#files=ImageTk.PhotoImage(Image.open('10.png'))
+
+frame1=LabelFrame(root,height=300,width=300,bg='#C2F77B',padx=50)
+
+frame1.pack(expand='Yes',side=TOP)
+
+Label1=Label(frame1,text="click the button to roll the dice:",bg='#C2F77B',font="Cambria")
+Label1.pack()
+
+button1=Button(frame1,bg='red',activebackground='blue',padx=50,command=number)
+button1.pack()
+
+
+root.mainloop()
